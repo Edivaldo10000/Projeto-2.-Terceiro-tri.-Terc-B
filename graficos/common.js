d82ceaa39c74784f25ccd0ac09f6039e1328b82b
Edivaldo10000 <129063675+Edivaldo10000@users.noreply.github.com>
2024-11-03 19:52:52 -0300

    aula 2

--- graficos/common.js
+++ graficos/common.js
@@ -1,3 +1,4 @@
+
 const getCSS = (variavel) => {
     return getComputedStyle(document.body).getPropertyValue(variavel)
 }
@@ -8,4 +9,15 @@ const tickConfig = {
     family: getCSS('--font')
 }
 
-export { getCSS, tickConfig }
+function criarGrafico(data, layout) {
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico';
+    document.getElementById('graficos-container').appendChild(grafico);
+    const config = {
+        responsive: true,
+        displayModeBar: false
+    }
+    Plotly.newPlot(grafico, data, layout, config);
+}
+
+export { getCSS, tickConfig, criarGrafico }
